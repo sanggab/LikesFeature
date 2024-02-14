7ebbb1d1cfe9247ab522e75a15f9f86bebc24e11
심상갑 <kappa2410@gmail.com>
2024-02-15 08:43:14 +0900

    Initial Commit

---
+++ LikesFeature/LikesFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikesFeatureApp.swift
+//  LikesFeature
+//
+//  Created by Gab on 2024/02/15.
+//
+
+import SwiftUI
+
+@main
+struct LikesFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
